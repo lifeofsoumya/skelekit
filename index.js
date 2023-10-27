2b916275d2da0079d16e8c6468d08829ad9d8d86
lifeofsoumya <mondalsoumya2001@gmail.com>
2023-10-27 13:27:44 +0530

    moved index inside src, did major chagnes in package.json

--- index.js
+++
@@ -1,1 +0,0 @@
-export { default as Skelekit } from './src/Skelekit';
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,7 +2,7 @@ var path = require('path')
 
 module.exports = {
   mode: 'production',
-  entry: './src/Skelekit.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
@@ -19,7 +19,15 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
       },
     ],
   },
